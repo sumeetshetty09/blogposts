b4278788bcf4a84721d2b573ebe3a6b27c7ad211
sumeetshetty09 <128564409+sumeetshetty09@users.noreply.github.com>
2024-01-20 22:44:45 +0530

    Add files via upload

---
+++ USER_POSTS_CRUD/posts/models.py
@@ -0,0 +1,9 @@
+# models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
